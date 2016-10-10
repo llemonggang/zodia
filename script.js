9d7136805ae3f1a070235676dae0a81fb06ecc3d
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-10-10 15:28:55 -0500

    working on icons

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ function phaseMoon() {
     }).done(function(data) {
     var moon = $('.moon').text(data.response['0'].moon.phase.name);
     return moon;
+    console.log(moon);
 
       if (moon === 'waxing gibbous') {
         console.log('working');
