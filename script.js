e803dbbfed16f6bbeb10b446d2cce423d01b27bb
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-10-10 12:45:55 -0500

    working on moonphase

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ function phaseMoon() {
           e.preventDefault()
             var output = document.getElementById("out");
           $.ajax({
-          url:'https://api.aerisapi.com/sunmoon/moonphases/?p=' + latitude + ',' + longitude + '&client_id=id5zV9deaqOeGuK3mYadq&client_secret=id5zV9deaqOeGuK3mYadq',
+          url:'https://api.aerisapi.com/sunmoon/moonphases/?p=austin,tx&client_id=id5zV9deaqOeGuK3mYadq&client_secret=id5zV9deaqOeGuK3mYadq',
           // today: today,
-          latitude: latitude,
-          longitude: longitude
+          // latitude: latitude,
+          // longitude: longitude
           }).done(function(data) {
           console.log(data);
           });
