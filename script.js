0c406eb87ac2998f19f044bf163e13c7361589a7
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-10-10 15:28:29 -0500

    working on icons

--- script.js
+++ script.js
@@ -45,32 +45,36 @@ function phaseMoon() {
     var moon = $('.moon').text(data.response['0'].moon.phase.name);
     return moon;
 
-      switch(moon) {
-        case 'waxing gibbous':
-            // $('.icon').html('<img src="./images/waxing-gibbous.png" />')
-            break;
-        case 'waning gibbous':
-            $('.icon').html('<img src="./images/waning-gibbous.png" />')
-            break;
-        case 'waxing crescent':
-            $('.icon').html('<img src="./images/waxing-crescent.png" />')
-            break;
-        case 'waning crescent':
-            $('.icon').html('<img src="./images/waning-crescent.png" />')
-            break;
-        case 'full moon':
-            $('.icon').html('<img src="./images/full.png" />')
-            break;
-        case 'new moon':
-            $('.icon').html('<img src="./images/new.png" />')
-            break;
-        case 'first moon':
-            $('.icon').html('<img src="./images/first.png" />')
-            break;
-        case 'last moon':
-            $('.icon').html('<img src="./images/last.png" />')
-            break;
-        }
+      if (moon === 'waxing gibbous') {
+        console.log('working');
+      }
+
+      // switch(moon) {
+      //   case 'waxing gibbous':
+      //       // $('.icon').html('<img src="./images/waxing-gibbous.png" />')
+      //       break;
+      //   case 'waning gibbous':
+      //       $('.icon').html('<img src="./images/waning-gibbous.png" />')
+      //       break;
+      //   case 'waxing crescent':
+      //       $('.icon').html('<img src="./images/waxing-crescent.png" />')
+      //       break;
+      //   case 'waning crescent':
+      //       $('.icon').html('<img src="./images/waning-crescent.png" />')
+      //       break;
+      //   case 'full moon':
+      //       $('.icon').html('<img src="./images/full.png" />')
+      //       break;
+      //   case 'new moon':
+      //       $('.icon').html('<img src="./images/new.png" />')
+      //       break;
+      //   case 'first moon':
+      //       $('.icon').html('<img src="./images/first.png" />')
+      //       break;
+      //   case 'last moon':
+      //       $('.icon').html('<img src="./images/last.png" />')
+      //       break;
+      //   }
 
     });
 
