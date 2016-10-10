38bac9672e2afcad3f9e6758130b217ada7cbe19
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-10-10 10:40:22 -0500

    testing geolocator

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
     apodImage()
-      getLoc()
     // phaseMoon
 
 
@@ -19,6 +18,8 @@ function apodImage() {
       return explanation;
   });
   })
+  // getLoc()
+  geoFindMe()
 }
 
 // function phaseMoon() {
@@ -37,5 +38,34 @@ function getLoc() {
   var loc = navigator.geolocation.watchPosition();
   // var latitude = position.coords.latitude;
   // var longitude = position.coords.longitude;
-  // console.log(loc);
+  console.log(loc);
+}
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
 }
