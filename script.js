eebf2af99cfe471cf95a254ec140005d93716009
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-10-12 12:43:12 -0500

    added sunrise/set

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     $('.daily').hide()
+    $('.back').hide()
     // apodImage()
     phaseMoon()
     horoscope()
@@ -20,7 +21,6 @@ function phaseMoon() {
   function success(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    console.log(latitude, longitude);
 
     var today = new Date();
     var dd = today.getDate();
@@ -42,7 +42,12 @@ function phaseMoon() {
     latitude: latitude,
     longitude: longitude
     }).done(function(data) {
+      console.log(data);
     var moon = $('.moon').text(data.response['0'].moon.phase.name);
+    var sunrise = ($('.sunrise').text(data.response['0'].sun.riseISO)).text();
+    var sunset = ($('.sunset').text(data.response['0'].sun.setISO)).text();
+    var rise = $('.sunrise').html('<p>rise ' + sunrise.substr(11, 8));
+    var set = $('.sunset').html('<p>set ' + sunset.substr(11, 8));
 
       switch(moon.html()) {
         case 'waxing gibbous':
@@ -78,115 +83,127 @@ function horoscope() {
   $.ajax({
   url:'http://www.api.littleastro.com/restserver/index.php/api/horoscope/readings/format/json',
   }).done(function(data) {
-  console.log(data);
 
     $('.cancer').click(function(e) {
       e.preventDefault()
       var cancer = data[3].Daily_Horoscope;
+      $('.sun-sign').html('<p>' + data[3].Sign + '</br>' + data[3].Info + '<p/>')
       $('.daily').html('<p>' + cancer + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.leo').click(function(e) {
       e.preventDefault()
       var leo = data[4].Daily_Horoscope;
+      $('.sun-sign').html('<p>' + data[4].Sign + '</br>' + data[4].Info + '</p>')
       $('.daily').html('<p>' + leo + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.virgo').click(function(e) {
       e.preventDefault()
       var virgo = data[5].Daily_Horoscope;
       $('.daily').html('<p>' + virgo + '<p/>')
+      $('.sun-sign').html('<p>' + data[5].Sign + '</br>' + data[5].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.libra').click(function(e) {
       e.preventDefault()
       var libra = data[6].Daily_Horoscope;
       $('.daily').html('<p>' + libra + '<p/>')
+      $('.sun-sign').html('<p>' + data[6].Sign + '</br>' + data[6].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.scorpio').click(function(e) {
       e.preventDefault()
       var scorpio = data[7].Daily_Horoscope;
       $('.daily').html('<p>' + scorpio + '<p/>')
+      $('.sun-sign').html('<p>' + data[7].Sign + '</br>' + data[7].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.sagittarius').click(function(e) {
       e.preventDefault()
       var sagittarius = data[8].Daily_Horoscope;
       $('.daily').html('<p>' + sagittarius + '<p/>')
+      $('.sun-sign').html('<p>' + data[8].Sign + '</br>' + data[8].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.capricorn').click(function(e) {
       e.preventDefault()
       var capricorn = data[9].Daily_Horoscope;
       $('.daily').html('<p>' + capricorn + '<p/>')
+      $('.sun-sign').html('<p>' + data[9].Sign + '</br>' + data[9].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.aquarius').click(function(e) {
       e.preventDefault()
       var aquarius = data[10].Daily_Horoscope;
       $('.daily').html('<p>' + aquarius + '<p/>')
+      $('.sun-sign').html('<p>' + data[10].Sign + '</br>' + data[10].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.pisces').click(function(e) {
       e.preventDefault()
       var pisces = data[11].Daily_Horoscope;
       $('.daily').html('<p>' + pisces + '<p/>')
+      $('.sun-sign').html('<p>' + data[11].Sign + '</br>' + data[11].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.aries').click(function(e) {
       e.preventDefault()
       var aries = data[0].Daily_Horoscope;
       $('.daily').html('<p>' + aries + '<p/>')
+      $('.sun-sign').html('<p>' + data[0].Sign + '</br>' + data[0].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.taurus').click(function(e) {
       e.preventDefault()
       var taurus = data[1].Daily_Horoscope;
       $('.daily').html('<p>' + taurus + '<p/>')
+      $('.sun-sign').html('<p>' + data[1].Sign + '</br>' + data[1].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
     $('.gemini').click(function(e) {
       e.preventDefault()
       var gemini = data[2].Daily_Horoscope;
       $('.daily').html('<p>' + gemini + '<p/>')
+      $('.sun-sign').html('<p>' + data[2].Sign + '</br>' + data[2].Info + '<p/>')
       $('.circle-container').hide();
+      $('.sun-sign').show()
       $('.daily').show();
-      console.log('working');
-
+      $('.back').show()
     })
   });
 }
