181d49eb03ca2d895f73f28c378553bdddd29f5b
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-11-12 09:57:04 -0600

    script file working again'

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,67 @@
 $( document ).ready(function() {
   $('#daily-blurb').hide();
   $('#navbar').hide();
-  phaseMoon();
-  infoBox();
+  navigator.geolocation.getCurrentPosition(success);
+
+  function success(position) {
+    console.log(position);
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+
+    var yy = today.getFullYear();
+      if(dd<10){
+          dd='0'+dd
+      }
+      if(mm<10){
+          mm='0'+mm
+      }
+    var today = dd+'/'+mm+'/'+yy;
+    var date = $('#date').html(today);
+
+    $.ajax({
+    url:'https://api.aerisapi.com/sunmoon/?p=' + latitude + ',' + longitude + '&to=:' + today + '&client_id=id5zV9deaqOeGuK3mYadq&client_secret=Yfvc6cceJpNkt5aqmJB5i30VV1WfmS6z75d3hWRy',
+    today: today,
+    latitude: latitude,
+    longitude: longitude
+    }).done(function(data) {
+    var moon = $('#moonphase').text(data.response['0'].moon.phase.name);
+    var sunrise = ($('#sunrise').text(data.response['0'].sun.riseISO)).text();
+    var sunset = ($('#sunset').text(data.response['0'].sun.setISO)).text();
+    var rise = $('#sunrise').html('<p>rise ' + sunrise.substr(11, 5));
+    var set = $('#sunset').html('<p>set ' + sunset.substr(11, 5));
+
+      switch(moon.html()) {
+        case 'waxing gibbous':
+            $('#moon-icon').html('<img src="./images/waxing-gibbous.png" id="moon-icon" />');
+            break;
+        case 'waning gibbous':
+            $('#moon-icon').html('<img src="./images/waning-gibbous.png" id="moon-icon" />')
+            break;
+        case 'waxing crescent':
+            $('#moon-icon').html('<img src="./images/waxing-crescent.png" id="moon-icon" />')
+            break;
+        case 'waning crescent':
+            $('#moon-icon').html('<img src="./images/waning-crescent.png" id="moon-icon" />')
+            break;
+        case 'full moon':
+            $('#moon-icon').html('<img src="./images/full.png" id="moon-icon" />')
+            break;
+        case 'new moon':
+            $('#moon-icon').html('<img src="./images/new.png" id="moon-icon" />')
+            break;
+        case 'first moon':
+            $('#moon-icon').html('<img src="./images/first.png" id="moon-icon" />')
+            break;
+        case 'last moon':
+            $('#moon-icon').html('<img src="./images/last.png" id="moon-icon"/>')
+            break;
+        }
+    });
+  }
 
   $.ajax({
   url:'https://zodia.herokuapp.com/zodiac',
@@ -38,17 +97,17 @@ $( document ).ready(function() {
       });
     };
 
-      $('.sign').click(function() {
-        var sign = $(this).data('sign');
-        var currentSign = data[key[sign]];
-        $('#sunmoon').hide();
-        $('#ring').hide();
-        $('#site-name').hide();
-        $('#navbar').show();
-        $('#sun-sign').html('<p>' + currentSign.Sign + '</p>');
-        $('#blurb-content').html('<p>' + currentSign.Daily_Horoscope + '</p>');
-        showContent(currentSign);
-      });
+    $('.sign').click(function() {
+      var sign = $(this).data('sign');
+      var currentSign = data[key[sign]];
+      $('#sunmoon').hide();
+      $('#ring').hide();
+      $('#site-name').hide();
+      $('#navbar').show();
+      $('#sun-sign').html('<p>' + currentSign.Sign + '</p>');
+      $('#blurb-content').html('<p>' + currentSign.Daily_Horoscope + '</p>');
+      showContent(currentSign);
+    });
 
     $('#circle-icon').click(function() {
       $('.current').removeClass( "current" );
@@ -79,81 +138,3 @@ $( document ).ready(function() {
   });
 
 });
-
-
-function phaseMoon() {
-
-  navigator.geolocation.getCurrentPosition(success);
-
-  function success(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1;
-
-    var yy = today.getFullYear();
-      if(dd<10){
-          dd='0'+dd
-      }
-      if(mm<10){
-          mm='0'+mm
-      }
-    var today = dd+'/'+mm+'/'+yy;
-    var date = $('#date').html(today);
-
-    $.ajax({
-    url:'https://api.aerisapi.com/sunmoon/?p=' + latitude + ',' + longitude + '&to=:' + today + '&client_id=id5zV9deaqOeGuK3mYadq&client_secret=Yfvc6cceJpNkt5aqmJB5i30VV1WfmS6z75d3hWRy',
-    today: today,
-    latitude: latitude,
-    longitude: longitude
-    }).done(function(data) {
-    var moon = $('#moonphase').text(data.response['0'].moon.phase.name);
-    var sunrise = ($('#sunrise').text(data.response['0'].sun.riseISO)).text();
-    var sunset = ($('#sunset').text(data.response['0'].sun.setISO)).text();
-    var rise = $('#sunrise').html('<p>rise ' + sunrise.substr(11, 8));
-    var set = $('#sunset').html('<p>set ' + sunset.substr(11, 8));
-
-      switch(moon.html()) {
-        case 'waxing gibbous':
-            $('#moon-icon').html('<img src="./images/waxing-gibbous.png" id="moon-icon" />');
-            break;
-        case 'waning gibbous':
-            $('#moon-icon').html('<img src="./images/waning-gibbous.png" id="moon-icon" />')
-            break;
-        case 'waxing crescent':
-            $('#moon-icon').html('<img src="./images/waxing-crescent.png" id="moon-icon" />')
-            break;
-        case 'waning crescent':
-            $('#moon-icon').html('<img src="./images/waning-crescent.png" id="moon-icon" />')
-            break;
-        case 'full moon':
-            $('#moon-icon').html('<img src="./images/full.png" id="moon-icon" />')
-            break;
-        case 'new moon':
-            $('#moon-icon').html('<img src="./images/new.png" id="moon-icon" />')
-            break;
-        case 'first moon':
-            $('#moon-icon').html('<img src="./images/first.png" id="moon-icon" />')
-            break;
-        case 'last moon':
-            $('#moon-icon').html('<img src="./images/last.png" id="moon-icon"/>')
-            break;
-        }
-    });
-  };
-}
-
-function infoBox() {
-  $('#moon-icon').click(function() {
-        $('#moonphase').show()
-        $('#sunrise').hide()
-        $('#sunset').hide()
-    });
-  $('#sun-icon').click(function() {
-    $('#sunrise').show()
-    $('#sunset').show()
-    $('#moonphase').hide()
-  });
-};
